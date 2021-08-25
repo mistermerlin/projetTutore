573d335b1581c3f2bf93d9a4089382a949e0929b
Datouloba Ouattara <datoulobaben@yahoo.com>
2021-08-26 01:25:24 +0200

    affichage des infos du permutations

--- src/Controller/PermutationController.php
+++ src/Controller/PermutationController.php
@@ -105,7 +105,7 @@ class PermutationController extends AbstractController
     public function view(): Response
     {
         $permutations = $this->getDoctrine()->getRepository(Permutation::class)->findBy(['statut' => '1'], []);
-        return $this->render('permutation/print.html.twig', [
+        return $this->render('permutation/view.html.twig', [
             'permutations' => $permutations
         ]);
     }
@@ -116,42 +116,8 @@ class PermutationController extends AbstractController
 
     public function print(Permutation $permutation)
     {
-        // Configure Dompdf according to your needs
-        $pdfOptions = new Options();
-        $pdfOptions->set('defaultFont', 'Arial');
-
-        // Instantiate Dompdf with our options
-        $dompdf = new Dompdf($pdfOptions);
-
-        // Retrieve the HTML generated in our twig file
-        $html = $this->renderView('pdf/mypdf.html.twig', [
-            'auteur' => $permutation->getAuteur('nom')
+        return $this->render('permutation/print.html.twig', [
+            'permutation' => $permutation
         ]);
-
-        // Load HTML to Dompdf
-        $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Store PDF Binary Data
-        $output = $dompdf->output();
-
-        // In this case, we want to write the file in the public directory
-        $publicDirectory = $this->get('kernel')->getProjectDir() . '/public';
-        // e.g /var/www/project/public/mypdf.pdf
-        $pdfFilepath =  $publicDirectory . '/permutation.pdf';
-
-        // Write file to the desired path
-        file_put_contents($pdfFilepath, $output);
-
-        // Send some text response
-        //return new Response("The PDF file has been succesfully generated !");
-        $this->addFlash('info', "impression terminer");
-        return $this->redirectToRoute('permutation');
-        //return $this->render('permutation/print.html.twig', []);
     }
 }
